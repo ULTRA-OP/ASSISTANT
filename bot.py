d8f0ba0428dd37ae6766425a80d317c89fa22546
ᴅɪᴘᴀɴsʜᴜ <77918121+infotechbro@users.noreply.github.com>
2021-04-12 07:52:11 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -86,11 +86,12 @@ async def back(event):
 
   await event.edit("**Hello I am UltraX assitant.\nA simple group manager bot to manage UltraX network**", buttons=PERU)
   
-@bot.on(events.NewMessage(pattern="/start|/START"))
-
+@bot.on(events.NewMessage(pattern="/rules|/RULES"))
 async def assistant (event):
-
   if event.chat_id == -1001427249400:
+     await bot.send_message(event.chat.id, "**Click below to check group rules**,buttons=hmm)
+  else:
+     await bot.send_message(event.chat.id, f"**Hello mate\nI am UltraX Assistant, Sorry to say that i only work in UltraX network.**", buttons=alain)
 
 
 logging.basicConfig(
